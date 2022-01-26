2589ce93f87459f8c7d9505d190c93a03f336155
salih <askd@gmail.com@>
2022-01-26 19:53:39 +0100

    hs8

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
         <footer>
             <ul>
-                <li><Link to="/"></Link> Home</li>
-                <li><Link to="/services"></Link>Services</li>
-                <li><Link to="/about"></Link>About Us</li>
-                <li><Link to="/contact"></Link>Contact</li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/services">Services</Link></li>
+                <li><Link to="/about">About Us</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
             </ul>
-            <div>&copy;2022 - Copyright by Blue Rental Cars Inc.</div>
+            <div>&copy;2021 - Copyright by Blue Rental Cars Inc.</div>
         </footer>
     )
-};
+}
 
-export default Footer;
+export default Footer
