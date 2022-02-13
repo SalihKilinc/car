8b2b94113b57c34b119b71f844d35cc04c52da8f
salih <askd@gmail.com@>
2022-02-13 22:25:18 +0100

    y1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from "react";
+
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import { getUser, login } from "./api/user-service";
 import Footer from "./components/common/Footer";
 import MenuBar from "./components/common/MenuBar";
 import TopBar from "./components/common/TopBar";
 import CustomRoutes from "./router/CustomRoutes";
+import { loginSuccess } from "./store/user/userActions";
+import {useStore} from "./store";
 
 
 const App = () => {
+
+  const {dispatchUser} = useStore();
+
+const loadData = async () => {
+  
+  try {
+
+    const respUser = await getUser();
+    if(respUser.status !== 200 )throw ("An error occurred while loading");
+    
+    dispatchUser(loginSuccess (respUser.data));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+
+useEffect(() => {
+
+  loadData();
+},[])
+
+
+
+
   return (
     <BrowserRouter>
       <TopBar />
--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const PrivateRoute = () => {
   return (
-
+<>
+</>
     
   )
-};
+}
 
 export default PrivateRoute;
